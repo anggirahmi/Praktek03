20d54dcfd68e555d71c861151050dcb0310d3e14
Anggii <anggirahmi07@gmail.com>
2018-04-06 20:27:36 +0800

    menggunakan method get

--- src/praktek03/TeleponAksi.java
+++ src/praktek03/TeleponAksi.java
@@ -8,5 +8,12 @@ public class TeleponAksi {
         s.setHarga(500000);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
