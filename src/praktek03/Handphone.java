87a42e21d2f7a77c186f9a0f250f53faadc67d97
Anggii <anggirahmi07@gmail.com>
2018-04-06 20:35:44 +0800

    attribute dan method

--- src/praktek03/Handphone.java
+++ src/praktek03/Handphone.java
@@ -1,5 +1,10 @@
 package praktek03;
 
 public class Handphone extends Telepon{
+    private String jenisHandphone;
+    
+    void throttle(){
+        System.out.println("Taktaktak.....)";
+    }
     
 }
