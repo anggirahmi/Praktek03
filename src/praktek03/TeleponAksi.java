d8e1adbe15c088d582af42e1e001dfc2b2802e8d
Anggii <anggirahmi07@gmail.com>
2018-04-06 19:39:04 +0800

    Instance buat atribut dan panggil method

--- src/praktek03/TeleponAksi.java
+++ src/praktek03/TeleponAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class TeleponAksi {
     public static void main(String[] args) {
+        Telepon s = new Telepon();
+        s.merk="Panasonic";
+        s.warna="Hitam";
+        s.harga=500000;
         
+        s.cetakInfo();
     }
 }
