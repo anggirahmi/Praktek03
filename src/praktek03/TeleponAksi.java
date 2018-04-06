31304a7beb5d9e1336bdf1ff1e003f74f76d76bc
Anggii <anggirahmi07@gmail.com>
2018-04-06 21:06:58 +0800

    Alhamdulillah selesai

--- src/praktek03/TeleponAksi.java
+++ src/praktek03/TeleponAksi.java
@@ -6,6 +6,7 @@ public class TeleponAksi {
         s.setMerk("Panasonic");
         s.setWarna("Hitam");
         s.setHarga(500000);
+        //gak bisa s.setJenisHandphone();
         
         s.cetakInfo();
         
@@ -15,5 +16,14 @@ public class TeleponAksi {
         System.out.println(s.getWarna());
         System.out.print("Harganya \t: ");
         System.out.println(s.getHarga());
+        
+        Handphone sh = new Handphone();
+        sh.setMerk("Samsung");
+        sh.setWarna("Putih");
+        sh.setHarga(200000);
+        sh.setJenisHandphone("Touchscreen");
+        sh.cetakInfo();
+        System.out.println("Jenis \t:"+sh.getJenisHandphone());
+        sh.throttle();
     }
 }
