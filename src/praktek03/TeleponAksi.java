b3e9ed6c0417c1e78a9656fa5e0a315a210fe978
Anggii <anggirahmi07@gmail.com>
2018-04-06 20:22:22 +0800

    menggunakan method set

--- src/praktek03/TeleponAksi.java
+++ src/praktek03/TeleponAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class TeleponAksi {
     public static void main(String[] args) {
         Telepon s = new Telepon();
-        s.merk="Panasonic";
-        s.warna="Hitam";
-        s.harga=500000;
+        
+        s.setMerk("Panasonic");
+        s.setWarna("Hitam");
+        s.setHarga(500000);
         
         s.cetakInfo();
     }
